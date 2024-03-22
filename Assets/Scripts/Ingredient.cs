64f52af4dffa33718c0352f8d83eb3da987b7ccf
MrSebi06 <mihneasebastian06@gmail.com>
2024-03-22 16:53:18 +0100

    Base grab mechanic

---
+++ Assets/Scripts/Ingredient.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Ingredient : MonoBehaviour
+{
+    [SerializeField] private string name;
+    public GameObject grabbed;
+    [SerializeField] private GameObject stack;
+}
